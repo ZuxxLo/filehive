f2f81ad9e1c4bdcde8e06d1ecddd057102158ab2
rougixxx <m.remmane@esi-sba.dz>
2024-04-08 01:04:13 +0100

    Fix the reset password route

--- code/filehive_auth/urls.py
+++ code/filehive_auth/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('auth/verify/<uidb64>/<token>', VerifyAccountView.as_view(), name='verify'),
     path("email-verified/<uidb64>/<token>", VerifyEmail.as_view(), name="email-verified" ),
     path("auth/receive-reset", SendResetEmail.as_view()),
-    path('auth/reset/<uidb64>/<token>', VerifyReset.as_view(), name='verify-reset'),
+    path('auth/verify-reset/<uidb64>/<token>', VerifyReset.as_view(), name='verify-reset'),
     path("reset-password/<uidb64>/<token>", ResetEmail.as_view(), name="reset-password"),
     path("auth/reset", ResetPasswordView.as_view(), name='reset' )
 
