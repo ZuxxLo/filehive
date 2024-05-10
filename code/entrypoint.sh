2ed3984d9da0cd40d789604e3d0fd3a9929d6464
rougixxx <m.remmane@esi-sba.dz>
2024-05-10 18:36:17 +0100

    Initial data loading problem is solved

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -11,19 +11,23 @@ python manage.py migrate
 
 # Load data fixtures
 
-echo "Loading data fixtures..."
+echo "Loading data fixtures................"
 
 
-for fixture_dir in fixtures/*; do
-    if [ -d "$fixture_dir" ]; then
-        for fixture_file in "$fixture_dir"/*.json; do
-            if [ -f "$fixture_file" ]; then
-                echo "Loading $fixture_file"
-                python manage.py loaddata "$fixture_file"
-            fi
-        done
-    fi
-done
+# for fixture_dir in fixtures/*; do
+#     if [ -d "$fixture_dir" ]; then
+#         for fixture_file in "$fixture_dir"/*.json; do
+#             if [ -f "$fixture_file" ]; then
+#                 echo "Loading $fixture_file"
+#                 python manage.py loaddata "$fixture_file"
+#             fi
+#         done
+#     fi
+# done
+echo "Loading Users*****************************"
+python manage.py loaddata fixtures/users/data.json
+echo "Loading Files*****************************"
+python manage.py loaddata fixtures/files/data.json
 
 
 echo "Starting Django development server......"
